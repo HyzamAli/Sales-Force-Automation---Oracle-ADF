6edde27c4b802779ff0636b8ba2121a8119439d2
hyzam <hyzam@192.168.29.105>
2020-12-14 16:10:43 +0530

    - Added Account - Tasks, Appointments

---
+++ ViewController/src/view/AttendeeBean.java
@@ -0,0 +1,16 @@
+package view;
+
+public class AttendeeBean {
+    private String employeeId;
+
+    public AttendeeBean() {
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+}
