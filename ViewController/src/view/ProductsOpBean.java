2b3b04588c273c4d7798312912f5f97e5ff4378f
hyzam <hyzam@192.168.29.105>
2021-01-20 20:15:19 +0530

    - major fixes

--- ViewController/src/view/ProductsOpBean.java
+++ ViewController/src/view/ProductsOpBean.java
@@ -5,6 +5,8 @@ import oracle.adf.model.binding.DCBindingContainer;
 import oracle.adf.view.rich.component.rich.RichDialog;
 import oracle.adf.view.rich.component.rich.RichPopup;
 
+import oracle.adf.view.rich.event.DialogEvent;
+
 import oracle.binding.OperationBinding;
 
 public class ProductsOpBean {
@@ -12,6 +14,7 @@ public class ProductsOpBean {
     private String quantity;
     private String category;
     private RichDialog component;
+    private RichDialog deleteComponent;
 
     public ProductsOpBean() {
     }
@@ -52,7 +55,48 @@ public class ProductsOpBean {
         DCBindingContainer bindings = (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
         OperationBinding operationBinding = bindings.getOperationBinding("Commit");
         operationBinding.execute();
+        operationBinding = bindings.getOperationBinding("Execute");
+        operationBinding.execute();
+        operationBinding = bindings.getOperationBinding("Execute1");
+        operationBinding.execute();
         RichPopup rp = (RichPopup) component.getParent();
         rp.hide();
+    }
+    
+    public void closeDeleteDialog() {
+        DCBindingContainer bindings = (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
+        OperationBinding operationBinding = bindings.getOperationBinding("Commit");
+        operationBinding.execute();
+        operationBinding = bindings.getOperationBinding("Execute");
+        operationBinding.execute();
+        operationBinding = bindings.getOperationBinding("Execute1");
+        operationBinding.execute();
+        RichPopup rp = (RichPopup) deleteComponent.getParent();
+        rp.hide();
+    }
+    
+    public void onUpdate(DialogEvent dialogEvent) {
+        DCBindingContainer bindings = (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
+        OperationBinding operationBinding;
+        if (dialogEvent.getOutcome() == DialogEvent.Outcome.yes)
+         {
+            operationBinding = bindings.getOperationBinding("Commit");
+            operationBinding.execute();
+            operationBinding = bindings.getOperationBinding("Execute");
+            operationBinding.execute();
+            operationBinding = bindings.getOperationBinding("Execute1");
+            operationBinding.execute();
+        } else {
+            operationBinding = bindings.getOperationBinding("Rollback");
+            operationBinding.execute();
+        }
+    }
+
+    public void setDeleteComponent(RichDialog deleteComponent) {
+        this.deleteComponent = deleteComponent;
+    }
+
+    public RichDialog getDeleteComponent() {
+        return deleteComponent;
     }
 }
